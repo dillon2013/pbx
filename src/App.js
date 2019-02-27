2be51d5623dd84bcc4be02d35836d384b18ec1b2
7loops <ales.maticic@gmail.com>
2019-02-27 16:08:46 +0000

    cleanup

--- src/App.js
+++ src/App.js
@@ -2,14 +2,6 @@ import React from 'react';
 
 const App = () => {
 
-    fetch('http://localhost:3000/api/products')
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (myJson) {
-            console.log(JSON.stringify(myJson));
-        });
-
     return (
         <main>
             Listing page
